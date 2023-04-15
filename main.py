c60b0df957870f0b4a528e4c853d9fc7af21b8f9
Shuang Wu <seanwu1105@gmail.com>
2023-04-15 14:45:34 -0400

    feat: use 3 isosurfaces to render vegetation (#1)

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from PySide6.QtWidgets import QApplication, QGridLayout, QMainWindow, QWidget
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from vtkmodules.vtkCommonColor import vtkNamedColors
 from vtkmodules.vtkIOXML import vtkXMLStructuredGridReader
-from vtkmodules.vtkRenderingCore import vtkActor, vtkDataSetMapper, vtkRenderer
+from vtkmodules.vtkRenderingCore import vtkRenderer
 
 from src.data import DATA_DIR, FILE_ID_MIN, to_filename
 from src.temporal import build_temporal_gui
+from src.vegetation import get_vegetation_actor
 from src.vtk_side_effects import import_for_rendering_core, import_for_rendering_volume
 from src.window import WINDOW_HEIGHT, WINDOW_WIDTH
 
@@ -27,14 +28,22 @@ window.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
 central = QWidget()
 layout = QGridLayout()
 layout.setColumnStretch(1, 1)
-temporal_label, temporal_gui = build_temporal_gui()
+temporal_label, temporal_spinbox = build_temporal_gui()
 layout.addWidget(temporal_label, 0, 0)
-layout.addWidget(temporal_gui, 0, 1, 1, -1)
+layout.addWidget(temporal_spinbox, 0, 1, 1, -1)
 
 ######## VTK Widget
 
 reader = vtkXMLStructuredGridReader()
 reader.SetFileName(str(DATA_DIR / to_filename(FILE_ID_MIN)))
+
+
+def on_time_changed(value: str):
+    reader.SetFileName(str(DATA_DIR / to_filename(int(value))))
+    vtk_widget.GetRenderWindow().Render()
+
+
+temporal_spinbox.textChanged.connect(on_time_changed)
 
 # Disable the arrays we don't need to save memory.
 reader.SetPointArrayStatus("u", 0)
@@ -47,14 +56,8 @@ reader.SetPointArrayStatus("rhof_1", 1)
 reader.SetPointArrayStatus("convht_1", 0)
 reader.SetPointArrayStatus("frhosiesrad_1", 0)
 
-mapper = vtkDataSetMapper()
-mapper.SetInputConnection(reader.GetOutputPort())
-
-actor = vtkActor()
-actor.SetMapper(mapper)
-
 renderer = vtkRenderer()
-renderer.AddActor(actor)
+renderer.AddActor(get_vegetation_actor(reader.GetOutputPort()))
 colors = vtkNamedColors()
 renderer.SetBackground(colors.GetColor3d("SlateGray"))  # type: ignore
 
