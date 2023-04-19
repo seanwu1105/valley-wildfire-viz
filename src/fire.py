48d28435dc1f415eb8d31881d69c3f6fbe0327a2
alexpjhsu <alexpjhsu@gmail.com>
2023-04-19 01:01:14 -0400

    on-the-fly fire volume rendering, very slow

--- src/fire.py
+++ src/fire.py
@@ -1,57 +1,89 @@
-from vtkmodules.vtkCommonCore import vtkLookupTable
-from vtkmodules.vtkCommonExecutionModel import vtkAlgorithmOutput
-from vtkmodules.vtkFiltersCore import vtkContourFilter
-from vtkmodules.vtkRenderingCore import vtkActor, vtkDataSetMapper, vtkRenderer
+from vtkmodules.vtkCommonDataModel import (
+    vtkStructuredGrid,
+    vtkImageData,
+    vtkPiecewiseFunction,
+    vtkDataObject,
+)
+from vtkmodules.vtkIOXML import vtkXMLStructuredGridReader
+from vtkmodules.vtkFiltersCore import vtkProbeFilter
+from vtkmodules.vtkRenderingCore import (
+    vtkVolumeProperty,
+    vtkColorTransferFunction,
+    vtkVolume,
+)
+from vtkmodules.vtkRenderingVolume import vtkGPUVolumeRayCastMapper
 
 
 # pylint: disable=too-many-arguments
-def create_layer(
-    port: vtkAlgorithmOutput,
-    isovalue: float,
-    r: float,
-    g: float,
-    b: float,
-    alpha: float,
-):
-    contours = vtkContourFilter()
-    contours.SetInputConnection(port)
-    contours.SetValue(0, isovalue)
+def create_image_data(data: vtkStructuredGrid):
+    bounds = data.GetBounds()
+    bounds = [round(x) for x in bounds]
+    dimensions = bounds[1::2]
+    spacing = (1.0, 1.0, 1.0)
+    origin = bounds[0::2]
 
-    lut = vtkLookupTable()
-    lut.SetNumberOfTableValues(1)
-    lut.SetTableValue(0, r, g, b, alpha)
-    lut.Build()
+    image_data = vtkImageData()
+    image_data.SetDimensions(dimensions)
+    image_data.SetSpacing(spacing)
+    image_data.SetOrigin(origin)
 
-    mapper = vtkDataSetMapper()
-    mapper.SetLookupTable(lut)
-    mapper.SetInputConnection(contours.GetOutputPort())
-
-    actor = vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().SetOpacity(alpha)
-
-    return actor
+    return image_data
 
 
-def add_flame_actor(port: vtkAlgorithmOutput, renderer: vtkRenderer):
-    isosurfaces = [
-        [400, 0.45, 0.0, 0.0, 0.5],
-        [500, 0.90, 0.0, 0.0, 0.6],
-        [600, 1.00, 0.8, 0.0, 0.7],
-        [700, 1.00, 1.0, 0.6, 0.8],
-        [800, 1.00, 1.0, 1.0, 0.9],
-    ]
+def add_fire_volume(reader: vtkXMLStructuredGridReader):
+    reader.Update()
+    image_data = create_image_data(reader.GetOutput())
 
-    for param in isosurfaces:
-        actor = create_layer(port, *param)
-        renderer.AddActor(actor)
+    probe_filter = vtkProbeFilter()
+    probe_filter.SetInputData(image_data)
+    probe_filter.SetSourceConnection(reader.GetOutputPort())
+    probe_filter.Update()
 
+    volumeProperty = vtkVolumeProperty()
+    colorTransferFunction = vtkColorTransferFunction()
+    opacityTransferFunction = vtkPiecewiseFunction()
 
-def add_smoke_actor(port: vtkAlgorithmOutput, renderer: vtkRenderer):
-    isosurfaces = [
-        [310, 0.96, 0.96, 0.96, 0.3],
-    ]
+    # flame
+    colorTransferFunction.AddRGBPoint(400 - 1, 0, 0, 0)
+    colorTransferFunction.AddRGBPoint(400, 0.45, 0.0, 0.0)
+    colorTransferFunction.AddRGBPoint(500, 0.90, 0.0, 0.0)
+    colorTransferFunction.AddRGBPoint(600, 1.00, 0.8, 0.0)
+    colorTransferFunction.AddRGBPoint(700, 1.00, 1.0, 0.6)
+    colorTransferFunction.AddRGBPoint(800, 1.00, 1.0, 1.0)
+    colorTransferFunction.AddRGBPoint(800 + 1, 0, 0, 0)
 
-    for param in isosurfaces:
-        actor = create_layer(port, *param)
-        renderer.AddActor(actor)
+    opacityTransferFunction.AddPoint(400 - 1, 0)
+    opacityTransferFunction.AddPoint(400, 0.5)
+    opacityTransferFunction.AddPoint(400, 0.6)
+    opacityTransferFunction.AddPoint(400, 0.7)
+    opacityTransferFunction.AddPoint(400, 0.8)
+    opacityTransferFunction.AddPoint(800, 0.9)
+    opacityTransferFunction.AddPoint(800 + 1, 0)
+
+    # smoke
+    colorTransferFunction.AddRGBPoint(310 - 1, 0, 0, 0)
+    colorTransferFunction.AddRGBPoint(310, 0.96, 0.96, 0.96)
+    colorTransferFunction.AddRGBPoint(310 + 1, 0, 0, 0)
+
+    opacityTransferFunction.AddPoint(310 - 1, 0)
+    opacityTransferFunction.AddPoint(310, 0.3)
+    opacityTransferFunction.AddPoint(310 + 1, 0)
+
+    volumeProperty.SetColor(colorTransferFunction)
+    volumeProperty.SetScalarOpacity(opacityTransferFunction)
+    volumeProperty.ShadeOn()
+    volumeProperty.SetInterpolationTypeToLinear()
+
+    volumeMapper = vtkGPUVolumeRayCastMapper()
+    volumeMapper.SetInputConnection(probe_filter.GetOutputPort())
+    volumeMapper.SetInputArrayToProcess(
+        0, 0, 0, vtkDataObject.FIELD_ASSOCIATION_POINTS, "theta"
+    )
+    volumeMapper.AutoAdjustSampleDistancesOff()
+    volumeMapper.SetSampleDistance(0.1)
+
+    volume = vtkVolume()
+    volume.SetMapper(volumeMapper)
+    volume.SetProperty(volumeProperty)
+
+    return volume
